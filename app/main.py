ac78dd9efd8c8e187dd74499c5008b3da286ed0a
Kauã <eukauapro@gmail.com>
2025-07-12 00:36:44 -0300

    Fix: Alterações nos arquivos index.html e main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://postgres:postgres@postgres-service:5432/postgres"
-engine = create_engine(DATABASE_URL)
+# Substitua pelo IP e porta do frontend ou use ["*"] temporariamente
+origins = [
+    "http://192.168.56.10:30001",  # frontend
+    "http://localhost:30001"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # ou ["*"] só para teste
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-def read_root():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT 'Conectado ao PostgreSQL com sucesso!'"))
-        return {"message": result.scalar()}
+def root():
+    return {"message": "Conectado ao PostgreSQL com sucesso!"}
